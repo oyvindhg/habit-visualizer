512b53c1a873e430f26535d8b332deb90bdc397f
Øyvind Harding Gulbrandsen <oyvindhg@gmail.com>
2025-02-09 20:57:28 +0900

    Add fitbit authentication

--- downloader.py
+++ downloader.py
@@ -3,27 +3,36 @@ import json
 import argparse
 from pathlib import Path
 from dotenv import load_dotenv
+from habit_visualizer.fitbit_client import FitbitClient
 from habit_visualizer.notion_client import NotionClient
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Tool to download habit tracker data")
     parser.add_argument('-y', '--year', type=int, default=2025, help="Year to visualize")
+    parser.add_argument('-w', '--website', choices=['notion', 'fitbit'], default='notion', help="Which website to download from")
     return parser.parse_args()
+
 
 def run():
     args = parse_arguments()
     year = args.year
-
+    website = args.website
     load_dotenv()
-    auth_key = os.getenv("NOTION_API_SECRET")
-    table_id = os.getenv(f"NOTION_TABLE_{year}_ID")
 
-    json_data = NotionClient(auth_key).get_data(table_id)
-
+    match website:
+        case 'notion':
+            auth_key = os.getenv("NOTION_API_SECRET")
+            table_id = os.getenv(f"NOTION_TABLE_{year}_ID")
+            data = NotionClient(auth_key).get_data(table_id)
+        case 'fitbit':
+            client_id = os.getenv("FITBIT_CLIENT_ID")
+            client_secret = os.getenv("FITBIT_CLIENT_SECRET")
+            data = FitbitClient(client_id=client_id, client_secret=client_secret, token_file="fitbit-tokens.json").get_data()
+    
     Path(f"data/{year}").mkdir(exist_ok=True)
-    with open(f"data/{year}/habit_data.json", "w", encoding="utf-8") as file:
-        json.dump(json_data, file)
+    with open(f"data/{year}/{website}_data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file)
 
 
 if __name__ == "__main__":
---
+++ habit_visualizer/fitbit_client.py
@@ -0,0 +1,106 @@
+from dataclasses import dataclass
+import json
+import os
+import time
+from typing import Any
+from urllib.parse import urlencode
+import base64
+import fitbit
+
+import requests
+
+@dataclass
+class FitbitClientSettings:
+    redirect_url: str = "https://example.com"
+    auth_base_url: str = "https://www.fitbit.com/oauth2/authorize"
+    token_url: str = "https://api.fitbit.com/oauth2/token"
+    scope: str = "activity heartrate location nutrition social weight sleep profile"
+
+class FitbitClient:
+    def __init__(self, client_id: str, client_secret: str, token_file: str):
+        self.settings = FitbitClientSettings
+        self.client_id = client_id
+        self.credentials = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+        if os.path.exists(token_file):
+            self.access_token = self._load_token(token_file)
+        else:
+            self.access_token = self._authorize(token_file)
+        
+    def _authorize(self, token_file: str):
+        redirect_url = self.settings.redirect_url
+        auth_base_url = self.settings.auth_base_url
+        auth_settings = {
+            "client_id": self.client_id,
+            "response_type": "code",
+            "redirect_uri": redirect_url,
+            "scope": self.settings.scope
+        }
+        auth_url = f"{auth_base_url}?{urlencode(auth_settings)}"
+        print(f"Authentication url: {auth_url}")
+        authorization_code = input("Login to Fitbit with the above URL, and paste the authorization code from the redirection URL:")
+
+        token_url = self.settings.token_url
+        headers = {
+            "Authorization": f"Basic {self.credentials}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        auth_body = {
+            "grant_type": "authorization_code",
+            "code": authorization_code,
+            "client_id": self.client_id,
+            "redirect_uri": redirect_url
+        }
+
+        response = requests.post(token_url, headers=headers, data=auth_body, timeout=30)
+
+        response.raise_for_status()
+        tokens = response.json()
+        self._save_tokens(tokens=tokens, token_file = token_file)
+        return tokens["access_token"]
+
+    def _save_tokens(self, tokens: str, token_file: str):
+        with open(token_file, "w", encoding="utf-8") as file:
+            json.dump(tokens, file)
+    
+    def _refresh_tokens(self, refresh_token, token_file: str) -> str:
+
+        headers = {
+            "Authorization": f"Basic {self.credentials}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        refresh_data = {
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "client_id": self.client_id
+        }
+
+        response = requests.post(self.settings.token_url, headers=headers, data=refresh_data, timeout=30)
+        response.raise_for_status()
+
+        tokens = response.json()
+
+        self._save_tokens(tokens=tokens, token_file=token_file)
+        return tokens["access_token"]
+
+    def _load_token(self, token_file: str) -> str:
+        file_modified_time = os.path.getmtime(token_file)
+        with open(token_file, "r", encoding="utf-8") as file:
+            tokens = json.load(file)
+        
+        if time.time() > file_modified_time + tokens["expires_in"]:
+            print("TOKEN EXPIRED - REFRESH")  # TODO: Test that this works
+            return self._refresh_tokens(refresh_token = tokens["refresh_token"], token_file=token_file)
+        return tokens["access_token"]
+        
+
+    def get_data(self) -> dict[str, Any]:
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}"
+        }
+        
+        url = "https://api.fitbit.com/1.2/user/-/sleep/date/today.json"
+
+        response = requests.get(url, headers=headers, timeout=30)
+        return response.json()
